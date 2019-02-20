c2427ea889a8b28351ff7f56a9ee9b18f744a8bd
Jay Carlson <nop@nop.com>
2019-02-20 11:20:40 -0500

    Update packaging for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name="bip39gen",
-    version="0.1.0",
+    name="bip39gen-nop",
+    version="0.1.2",
     packages=find_packages(),
 
     package_data={
@@ -12,6 +17,15 @@ setup(
     author="Jay Carlson",
     author_email="nop@nop.com",
     description="Generate random BIP-039 wordlists",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
     license="MIT",
     keywords="bip39 bip039",
     url='https://github.com/nopdotcom/bip39gen',
